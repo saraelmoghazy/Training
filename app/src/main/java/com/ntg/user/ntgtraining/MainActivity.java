42185bfe8fa2fdd51eb4ee8b62c238c9eb7afbe9
Hend-iti <hendibrahim@hotmail.com>
2018-02-20 17:57:38 +0200

    Commit Hend

--- app/src/main/java/com/ntg/user/ntgtraining/MainActivity.java
+++ app/src/main/java/com/ntg/user/ntgtraining/MainActivity.java
@@ -3,6 +3,7 @@ package com.ntg.user.ntgtraining;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         ////////////////////////
         Toast.makeText(this, "message", Toast.LENGTH_LONG).show();
         Log.i("tag", "tag");
+        TextView tv = findViewById(R.id.tvTxtView);
+        tv.setText("Hello There");
 
     }
 }
