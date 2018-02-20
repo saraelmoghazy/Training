3231812ffb733f1254dc2f8a3fa9cf098ffc9ac4
nafea <a.nafea90@gmail.com>
2018-02-20 17:42:40 +0200

    nafea commit log4

--- app/src/main/java/com/ntg/user/ntgtraining/MainActivity.java
+++ app/src/main/java/com/ntg/user/ntgtraining/MainActivity.java
@@ -2,6 +2,7 @@ package com.ntg.user.ntgtraining;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i("nafea","hello team");
 
     }
 }
