2a40a66fc3e8385607fd1bd2f00b68fca7bd23ce
nafea <a.nafea90@gmail.com>
2018-02-20 17:36:09 +0200

    nafea commit log2

--- app/src/main/java/com/ntg/user/ntgtraining/MainActivity.java
+++ app/src/main/java/com/ntg/user/ntgtraining/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"hello nafea",Toast.LENGTH_LONG).show();
-        Log.i("Hello","go home");
+        Log.i("Hello","go home ya nafea");
+
     }
 }
